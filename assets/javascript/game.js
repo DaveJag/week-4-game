7698dbcd164982e95e22faf6df65a5fe38a78fa3
David Jagodowski <davejag@comcast.net>
2017-07-06 14:07:33 -0400

    This build has functioning target number generatora, jewel images, modified css.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,22 @@
 /* David Jagodowski - HW04 */
-$(function)()  {  // Document ready function. Waits until HTML page loads before running this script
+$(document).ready(function()  {  // Document ready function. Waits until HTML page loads before running this script
+
+  //Begin Main Program
+  getRandomTargetNum();
+
+  //End Main Program
+
+
+  function getRandomTargetNum() {
+    //Generate a Random whole number between 19 and 120 
+    var x = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+    // alert(x); 
+    // Append to targetNumDiv
+    $("#targetNumDiv").append("<h2> " + x + " </h2>");
+    //alert("Is the program getting here?");
+
+};
 
 
 
-
-} // end document ready
+}); // end document ready
